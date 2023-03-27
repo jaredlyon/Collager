a7beb601b4779c41327427beb4b81ddcbe0b7d7b
Jared Lyon <jaredalyon@gmail.com>
2023-03-26 20:11:22 -0400

    main

--- A4/src/Main.java
+++ A4/src/Main.java
@@ -1,10 +1,13 @@
 import java.io.InputStreamReader;
 
+import collage.controller.GUIController;
 import collage.controller.ScriptController;
 import collage.controller.IController;
+import collage.model.GUIModel;
 import collage.model.IModel;
 import collage.model.Model;
 import collage.view.IView;
+import collage.view.JFrameView;
 import collage.view.View;
 
 /**
@@ -16,10 +19,23 @@ public final class Main {
    * @param args - a String[] determining which version to play
    */
   public static void main(String[] args) {
-    IModel model = new Model();
-    IView view = new View(model);
-    Readable in = new InputStreamReader(System.in);
-    IController controller = new ScriptController(model, view, in);
-    controller.startCollage();
+    if (args.length > 0) {
+      for (String argument : args) {
+        if (argument.equals("script")) {
+          IModel model = new Model();
+          IView view = new View(model);
+          Readable in = new InputStreamReader(System.in);
+          IController controller = new ScriptController(model, view, in);
+          controller.startCollage();
+        } else if (argument.equals("gui")) {
+          GUIModel model = new GUIModel();
+          JFrameView view = new JFrameView();
+          GUIController controller = new GUIController(model, view);
+          view.setController(controller);
+        }
+      }
+    } else {
+      System.out.println("No arguments found.");
+    }
   }
 }
